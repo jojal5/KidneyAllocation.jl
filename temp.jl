c4c584f2a75e16cb842d5421eb9e5b5a6ad6af7a
Jonathan Jalbert <39426670+jojal5@users.noreply.github.com>
2025-12-10 14:30:33 -0500

    add possible outcomes to recipients

--- temp.jl
+++ temp.jl
@@ -1,4 +1,12 @@
 using KidneyAllocation
 using Test
 
-using Dates
+using Dates, CSV, DataFrames
+
+df = CSV.read("/Users/jalbert/Documents/PackageDevelopment.nosync/kidney-research/kidney_research/KidneyResearch/data/Candidates.csv", DataFrame)
+
+unique(df.OUTCOME)
+
+df_outcome = unique(select(df, :OUTCOME))
+
+CSV.write("outcome.csv", df_outcome)
